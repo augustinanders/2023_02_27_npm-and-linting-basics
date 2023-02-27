2bc9de514bbee7334e58d388c8e2c1f2908bdfa5
Augustin Anders <augustinanders@gmail.com>
2023-02-27 16:05:50 +0100

    fix bugs

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
+  const cards = document.querySelectorAll('[data-js=card]');
   cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
+    const button = card.querySelector('[data-js="button"]');
+    const link = card.querySelector('[data-js="link"]');
+    const arrow = card.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hidden');
+      arrow.classList.toggle('rotate');
+    });
+  });
 }
+toggleButton();
